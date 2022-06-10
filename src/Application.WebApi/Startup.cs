26632b434f2ffee84b96fe1f3e4bd6c1b6d87c4c
Zain Nabi <68240955+zain-nabi@users.noreply.github.com>
2022-06-10 06:43:35 +0200

    Update Startup.cs

--- src/Application.WebApi/Startup.cs
+++ src/Application.WebApi/Startup.cs
@@ -63,7 +63,7 @@ namespace Application.WebApi
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sales API V1");
-                //c.RoutePrefix = string.Empty;
+                c.RoutePrefix = string.Empty;
             });
         }
     }
